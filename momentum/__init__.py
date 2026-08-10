47ca57747f51c2a18d634cb9e7df76dc3dab3019
Doodles <moraes.eduardo@proton.me>
2026-08-10 18:42:36 -0300

    feat: adiciona pacote momentum

---
+++ momentum/__init__.py
@@ -0,0 +1,7 @@
+# Momentum - visualização do campo de momentos de uma força
+# Copyright (C) 2026 Eduardo Lima Moraes
+# Licenciado sob a GNU GPL v3 ou posterior; veja o arquivo LICENSE.
+
+"""Momentum: desenha o campo de momentos de uma força no espaço."""
+
+__version__ = "1.0.0"
