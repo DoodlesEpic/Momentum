e283f9005a9692b50da2ef1cee74d9caa190a944
Doodles <moraes.eduardo@proton.me>
2026-08-18 19:57:05 -0300

    refactor: adiciona o pacote comum

---
+++ momentum/comum/__init__.py
@@ -0,0 +1,5 @@
+# Momentum - programas das tarefas optativas de PME0100 Mecânica I
+# Copyright (C) 2026 Eduardo Lima Moraes
+# Licenciado sob a GNU GPL v3 ou posterior. Veja o arquivo LICENSE.
+
+"""Funções compartilhadas pelas tarefas do Momentum."""
