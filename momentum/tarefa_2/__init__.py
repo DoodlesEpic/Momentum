a972a9f96dfce3e78ac616aa90a53a2af7eb1736
Doodles <moraes.eduardo@proton.me>
2026-08-18 20:06:04 -0300

    feat: adiciona o pacote da tarefa 2

---
+++ momentum/tarefa_2/__init__.py
@@ -0,0 +1,5 @@
+# Momentum - programas das tarefas optativas de PME0100 Mecânica I
+# Copyright (C) 2026 Eduardo Lima Moraes
+# Licenciado sob a GNU GPL v3 ou posterior. Veja o arquivo LICENSE.
+
+"""Implementação da Tarefa Optativa 2."""
